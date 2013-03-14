977cf25dbca49ba2b947bb00ff8950c67cf0010e
Alessandro Gnoli <tebemis@gmail.com>
2013-03-14 16:04:19 -0500

    Remove and update events are now meaningful

--- src/main/java/ltg/ps/helioroom/HelioRoomSimulatorAgent.java
+++ src/main/java/ltg/ps/helioroom/HelioRoomSimulatorAgent.java
@@ -3,6 +3,9 @@
  */
 package ltg.ps.helioroom;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 import ltg.commons.LTGEvent;
@@ -22,9 +25,10 @@ public class HelioRoomSimulatorAgent {
 	public static String[] events = {"insert", "update", "remove"};
 
 	private LTGEventHandler eh = null;
+	private List<LTGEvent> generated_events = new ArrayList<LTGEvent>();
 
 
-	public HelioRoomSimulatorAgent(String usernameAndPass, String chatAndDBId, String[] r_events, int sleep) {
+	public HelioRoomSimulatorAgent(String usernameAndPass, String chatAndDBId, String[] r_events, int sleep, int stablePoint) { 
 
 		// -------------------
 		// Init network and DB
@@ -43,7 +47,7 @@ public class HelioRoomSimulatorAgent {
 		// Now we can simulate the main thread  and make ourselves busy
 		while(true) {
 			try {
-				generateRandomEvent(r_events);
+				generateRandomEvent(r_events, stablePoint);
 				Thread.sleep(sleep);
 			} catch (InterruptedException e) {
 				eh.close();
@@ -52,11 +56,49 @@ public class HelioRoomSimulatorAgent {
 	}
 
 
-	private void generateRandomEvent(String[] r_events) {
+	private void generateRandomEvent(String[] r_events, int stablePoint) {
 		if (r_events.length==events.length)
 			simulateTraffic();
-		else
-			chooseAmongAvailable(r_events);
+		else if (Arrays.asList(r_events).contains("insert") && Arrays.asList(r_events).contains("remove")) {
+			generateInsertsAndRemove(stablePoint);
+		} else if (Arrays.asList(r_events).contains("insert") && Arrays.asList(r_events).contains("update")) {
+			generateInsertAndUpdate(stablePoint);
+		} else if (Arrays.asList(r_events).contains("update") && Arrays.asList(r_events).contains("remove")){
+			System.out.println("This mode is not supported, exiting..,");
+			System.exit(0);
+		} else if (r_events.length==1 && r_events[0].equals("insert")) {
+			generateUpdateEvent();
+		} else if (r_events.length==1 && r_events[0].equals("update")) {
+			System.out.println("This mode is not supported, exiting..,");
+			System.exit(0);
+		} else if (r_events.length==1 && r_events[0].equals("remove")) {
+			System.out.println("This mode is not supported, exiting..,");
+			System.exit(0);
+		}
+	}
+
+
+	private void generateInsertAndUpdate(int stablePoint) {
+		if (generated_events.size() < stablePoint)
+			generateCreateEvent();
+		else {
+			if (Math.random() < 0.5) 
+				generateCreateEvent();
+			else
+				generateUpdateEvent();
+		}
+	}
+
+
+	private void generateInsertsAndRemove(int stablePoint) {
+		if (generated_events.size() < stablePoint)
+			generateCreateEvent();
+		else {
+			if (Math.random() < 0.5) 
+				generateCreateEvent();
+			else
+				generateRemoveEvent();
+		}
 	}
 
 
@@ -67,18 +109,7 @@ public class HelioRoomSimulatorAgent {
 		else if (r <0.75) 
 			generateCreateEvent();
 		else 
-			generateDeleteEvent();
-	}
-	
-	
-	private void chooseAmongAvailable(String[] r_events) {
-		String e = r_events[new Random().nextInt(r_events.length)];
-		if (e.equals("insert"))
-			generateCreateEvent();
-		if (e.equals("update"))
-			generateUpdateEvent();
-		if (e.equals("remove"))
-			generateDeleteEvent();
+			generateRemoveEvent();
 	}
 
 
@@ -102,52 +133,33 @@ public class HelioRoomSimulatorAgent {
 		payload.put("color", color);
 		payload.put("reason", reason);
 		eh.generateEvent(new LTGEvent(event, origin, null, payload));
+		generated_events.add(new LTGEvent(event, origin, null, payload));
 	}
 
 
-	private void generateDeleteEvent() {
-		String origin = generateOrigin();
+	private void generateRemoveEvent() {
+		Random r = new Random();
+		LTGEvent re = generated_events.remove(r.nextInt(generated_events.size()));
 		String event;
-		String color;
-		String anchor;
 		if (Math.random()<0.5) {
 			event = "remove_observation";
-			color = colors[(int) (Math.random()*colors.length)];
-			anchor = colors[(int) (Math.random()*colors.length)];
 		} else {
 			event = "remove_theory";
-			color = colors[(int) (Math.random()*colors.length)];
-			anchor = planets[(int) (Math.random()*planets.length)];
 		}
-		String reason = generateString((int) (Math.random()*100));
-		ObjectNode payload =  JsonNodeFactory.instance.objectNode();
-		payload.put("anchor", anchor);
-		payload.put("color", color);
-		payload.put("reason", reason);
-		eh.generateEvent(new LTGEvent(event, origin, null, payload));	
+		eh.generateEvent(new LTGEvent(event, re.getOrigin(), null, re.getPayload()));	
 	}
 
 
 	private void generateUpdateEvent() {
-		String origin = generateOrigin();
+		Random r = new Random();
+		LTGEvent re = generated_events.remove(r.nextInt(generated_events.size()));
 		String event;
-		String color;
-		String anchor;
 		if (Math.random()<0.5) {
 			event = "update_observation";
-			color = colors[(int) (Math.random()*colors.length)];
-			anchor = colors[(int) (Math.random()*colors.length)];
 		} else {
 			event = "update_theory";
-			color = colors[(int) (Math.random()*colors.length)];
-			anchor = planets[(int) (Math.random()*planets.length)];
 		}
-		String reason = generateString((int) (Math.random()*100));
-		ObjectNode payload =  JsonNodeFactory.instance.objectNode();
-		payload.put("anchor", anchor);
-		payload.put("color", color);
-		payload.put("reason", reason);
-		eh.generateEvent(new LTGEvent(event, origin, null, payload));
+		eh.generateEvent(new LTGEvent(event, re.getOrigin(), null, re.getPayload()));
 	}
 
 
@@ -174,10 +186,10 @@ public class HelioRoomSimulatorAgent {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// String[] events = {"insert", "update", "remove"};
-		String[] events = {"insert"};
-		new HelioRoomSimulatorAgent("biden", "android-test", events, 500);
-		//new HelioRoomSimulatorAgent("hr-simulator", "helio-sp-13", events, 1000);
+		String[] events = {"insert", "remove"};
+		//String[] events = {"insert"};
+		//new HelioRoomSimulatorAgent("biden", "android-test", events, 500);
+		new HelioRoomSimulatorAgent("hr-simulator", "helio-sp-13", events, 1000, 5*2);
 	}
 
 }
